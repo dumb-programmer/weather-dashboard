e4aeac65376523062c4637c6696fe2f0db270014
Asad Khan <asadkhan6192@gmail.com>
2024-06-18 23:15:50 +0500

    Make layout responsive

--- src/App.tsx
+++ src/App.tsx
@@ -26,16 +26,16 @@ function App() {
     <div className="h-screen w-full flex justify-center items-center">
       <div className="flex flex-col gap-4 shadow-md shadow-slate-500 p-10 bg-slate-900 text-slate-50 rounded-md">
         <CitySearch isLoading={isLoading} isError={isError} city={city} setCity={setCity} handleSubmit={handleSubmit} />
-        <div className="flex gap-6">
-          <MainInfoCard weather={weather} weather_description={weather_description} temp={temp} location={location} />
-          <div className="flex-1 grid grid-cols-2 gap-3">
-            <InfoCard Icon={<IconPennant />} title="Wind Speed" value={wind && `${wind} km/h` || "N/A"} />
-            <InfoCard Icon={<IconGauge />} title="Pressure" value={pressure && `${pressure} hPA` || "N/A"} />
-            <InfoCard Icon={<IconSunrise />} title="Sunrise" value={sunrise && sunrise && formatTime(sunrise, timezone) || "N/A"} />
-            <InfoCard Icon={<IconDroplet />} title="Humidity" value={humidity && `${humidity}%` || "N/A"} />
-            <InfoCard Icon={<IconMist />} title="Visibility" value={visibility && `${visibility / 1000} km` || "N/A"} />
-            <InfoCard Icon={<IconSunset />} title="Sunset" value={sunset && sunset && formatTime(sunset, timezone) || "N/A"} />
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+          <div className="col-start-1 col-span-2 md:row-start-1 md:row-span-2 md:col-start-1 md:col-span-1">
+            <MainInfoCard weather={weather} weather_description={weather_description} temp={temp} location={location} />
           </div>
+          <InfoCard Icon={<IconPennant />} title="Wind Speed" value={wind && `${wind} km/h` || "N/A"} />
+          <InfoCard Icon={<IconGauge />} title="Pressure" value={pressure && `${pressure} hPA` || "N/A"} />
+          <InfoCard Icon={<IconSunrise />} title="Sunrise" value={sunrise && sunrise && formatTime(sunrise, timezone) || "N/A"} />
+          <InfoCard Icon={<IconDroplet />} title="Humidity" value={humidity && `${humidity}%` || "N/A"} />
+          <InfoCard Icon={<IconMist />} title="Visibility" value={visibility && `${visibility / 1000} km` || "N/A"} />
+          <InfoCard Icon={<IconSunset />} title="Sunset" value={sunset && sunset && formatTime(sunset, timezone) || "N/A"} />
         </div>
       </div>
     </div >
